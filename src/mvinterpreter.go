fe54308457a43e5b08c48beee8cce3f92ecf0f4e
Ryken <rykenthompson@icloud.com>
2018-10-16 21:48:27 -0700

    Add Go Modules

--- src/mvinterpreter.go
+++ src/mvinterpreter.go
@@ -1,22 +1,51 @@
-package villager
+package main
 
 import (
 	"fmt"
 	"bufio"
 	"os"
 	"io"
+	"string"
 	"github.com/tmthrgd/go-memset"
 )
 
 var program []int
 
-func interpret(input string) bool {
+func interpret(input string) {
+	mcount := strings.Count(input, "m")
+	switch m {
+	case 2:
 
+	case 3:
+
+	case 4:
+
+	case 5:
+
+	case 6:
+
+	case 7:
+
+	case 8:
+
+	case 9:
+
+	case 10:
+
+	case 11:
+
+	case 12:
+
+	case 13:
+
+	default:
+		fmt.Printf("ERROR. Invalid command.\n")
+	}
 	return false
 }
 
 func main() {
-	file, err := os.Open(os.Args[1])
+	file, err := os.Open(os.Args[0])
 	if err != nil {
 		fmt.Println("Cannot open source file")
 		os.Exit(1)
@@ -36,16 +65,15 @@ func main() {
 	*/
 	read := true
 	for read {
-		n, readerr := reader.ReadBytes()
+		n, readerr := reader.ReadBytes('\n')
 		if readerr == io.EOF {
 			//do nothing yet
 			read = false
 		} else if readerr != nil {
-			fmt.Printf("Read Error")
+			fmt.Printf("Read Error, %d amount of bytes read.\n", n)
 			break
 		}
-		var cmd int = 0
 
-		interpret(string(buf[:n]))
+		interpret(string(buf))
 	}
 }
