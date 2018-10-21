114fe8d5fb3ec68f8afbe9e1e0f3ce2ea9c177a3
Ryken <rykenthompson@icloud.com>
2018-10-20 22:40:23 -0700

    Decreased cyclomatic complexity and did I mention it works now.

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # Minecraft_Villager
-[![Go Report Card](https://goreportcard.com/badge/github.com/wasd424/Minecraft_Villager)](https://goreportcard.com/report/github.com/wasd424/Minecraft_Villager) [![CodeFactor](https://www.codefactor.io/repository/github/wasd424/minecraft_villager/badge)](https://www.codefactor.io/repository/github/wasd424/minecraft_villager) [![Build Status](https://travis-ci.org/wasd424/Minecraft_Villager.svg?branch=master)](https://travis-ci.org/wasd424/Minecraft_Villager) 
+[![Go Report Card](https://goreportcard.com/badge/github.com/wasd424/Minecraft_Villager)](https://goreportcard.com/report/github.com/wasd424/Minecraft_Villager) [![CodeFactor](https://www.codefactor.io/repository/github/wasd424/minecraft_villager/badge)](https://www.codefactor.io/repository/github/wasd424/minecraft_villager) [![Build Status](https://travis-ci.org/wasd424/Minecraft_Villager.svg?branch=master)](https://travis-ci.org/wasd424/Minecraft_Villager)
 ## Get your fellow Minecraft villagers coding.
 A new esoteric programming language for Minecraft villagers. We all know that Minecraft Villagers only speak the word "_hmmm_." Thats why in this language all commands are versions of the word "hmm."
 
 ### This language is capable of:
-Right now, absolutely nothing. I haven't even finished the interpreter.
+It is extremely capable if you have the knowledge of a Minecraft villager or if you have bovine blood (Just look at the source and then compare it to COW's interpreter, you'll see).
+
+#### Available commands (Instruction code, Name, Description):
+1. hmm		  - Interpreter will search backwards from here to find the corresponding "hmmmmmmm" command and resume execution from there.
+2. hmmm		  - Moves current memory position forward.
+3. hmmmm	  - Moves current memory position backwards.
+4. hmmmmm	  - Executes the value in current memory block as if it was a normal instruction code.
+5. hmmmmmm	  - Reads single byte from input if current memory block is 0. Otherwise, prints current memory block value as a character.
+6. hmmmmmmm	  - Decreases value of current memory block by 1.
+7. hmmmmmmmm	  - Increases value of current memory block by 1.
+8. hmmmmmmmmm	  - If current memory block is 0 skip next command and resume after next "hmm". Otherwise, continue with execution.
+9. hmmmmmmmmmm	  - Set current memory block value to 0.
+10. hmmmmmmmmmmm   - If no value in current register, copy current memory block value into register. If there is a value in current register copy value into current memory block.
+11. hmmmmmmmmmmm  - Print value of current memory block as an integer.
+12. hmmmmmmmmmmmm - Read interger and put into current memory block.
+
+###### Notice:
+The instruction code is actually the number above minus 1. For example, the instruction code for "hmm" is 0.
 
 ### License
 All code is licensed under the GPL v3 license. See the license file for more info.
--- interpreter/mvinterpreter.go
+++ interpreter/mvinterpreter.go
@@ -1,5 +1,22 @@
 package interpreter
 
+/*
+Copyright 2018 Ryken Thompson (wasd424)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import (
 	"fmt"
 	"bufio"
@@ -16,93 +33,82 @@ var (
 	program = make([]int, 0)
 
 	// Program counter is the current position in program[].
-	pc = 0
+	pc int
 
 	mem = make([]int, 0) //Program Memory
-	memPos = 0 //Memory Position
+	memPos int //Memory Position
 
 	regsVal int
 	isRegsVal bool = false
 )
 
-func exec(instruction int) {
-	reader := bufio.NewReader(os.Stdin)
 
-	switch instruction {
+func cmd0() {
+	if pc == 0 {
+		fmt.Printf("Don't use \"hmm\" as the first cmd.\n")
+		os.Exit(1)
+	}
 
-	// hmm
-	case 0:
+	pc--
+
+	lvl := 1
+	for lvl > 0 {
 		if pc == 0 {
-			fmt.Printf("Don't use \"hmm\" as the first cmd.\n")
-			os.Exit(1)
+			break
 		}
 
 		pc--
 
-		lvl := 1
-		for lvl > 0 {
-			if pc == 0 {
-				break
-			}
-
-			pc--
-
-			if program[pc] == 0 {
-				lvl++
-			} else if program[pc] == 7 {
-				lvl--
-			}
+		if program[pc] == 0 {
+			lvl++
+		} else if program[pc] == 7 {
+			lvl--
 		}
-		if lvl != 0 {
-			fmt.Printf("Error.\n")
-			os.Exit(1)
-		}
-		exec(program[pc])
+	}
+	if lvl != 0 {
+		fmt.Printf("Error.\n")
+		os.Exit(1)
+	}
+	exec(program[pc])
+}
 
-	//hmmm
-	case 1:
-		if memPos == 0 {
-			os.Exit(1)
-		} else {
-			memPos--
-		}
+func cmd1() {
+	if memPos == 0 {
+		os.Exit(1)
+	} else {
+		memPos--
+	}
+}
 
-	//hmmmm
-	case 2:
-		memPos++
-		if memPos == len(mem) {
-			mem = append(mem, 0)
-			memPos = len(mem) - 1
-		}
+func cmd2() {
+	memPos++
+	if memPos == len(mem) {
+		mem = append(mem, 0)
+		memPos = len(mem) - 1
+	}
+}
 
-	// hmmmmm
-	case 3:
-		if mem[memPos] == 3 {
-			fmt.Printf("Error.\n")
-			os.Exit(1)
-		}
-		exec(mem[memPos])
+func cmd3() {
+	if mem[memPos] == 3 {
+		fmt.Printf("Error.\n")
+		os.Exit(1)
+	}
+	exec(mem[memPos])
+}
 
-	// hmmmmmm
-	case 4:
-		if mem[memPos] != 0 {
-			fmt.Printf("%c", mem[memPos])
-		} else {
-			input, _ := reader.ReadByte()
-			mem[memPos] = int(input)
-		}
+func cmd4(reader *bufio.Reader) {
+	if mem[memPos] != 0 {
+		fmt.Printf("%c", mem[memPos])
+	} else {
+		input, _ := reader.ReadByte()
+		mem[memPos] = int(input)
+	}
+}
 
-	// hmmmmmmm
-	case 5:
-		mem[memPos]--
+// func cmd5 and cmd6 wouldn't add any readability
 
-	// hmmmmmmmm
-	case 6:
-		mem[memPos]++
-
-	// hmmmmmmmmm
-	case 7:
-		if mem[memPos] == 0 {
+func cmd7() {
+	if mem[memPos] == 0 {
 
 			lvl := 1
 			prev := 0
@@ -117,18 +123,97 @@ func exec(instruction int) {
 
 				if program[pc] == 7 {
 					lvl++
-					} else if program[pc] == 0 {
+				} else if program[pc] == 0 {
+					lvl--
+					if prev == 7 {
 						lvl--
-						if prev == 7 {
-							lvl--
-						}
 					}
+				}
 			}
 			if lvl != 0 {
 				fmt.Printf("Error. Check your code.\n")
 				os.Exit(1)
 			}
+	}
+}
+
+// func cmd8 wouldn't add any readability
+
+func cmd9() {
+	if isRegsVal {
+		mem[memPos] = regsVal
+	} else {
+		regsVal = mem[memPos]
+	}
+	isRegsVal = !isRegsVal
+}
+
+// func cmd10 wouldn't add any readability
+
+func cmd11(reader *bufio.Reader) {
+	buf := make([]byte, 100)
+	c := 0
+
+	var err error
+
+	for c < len(buf)-1 {
+		input, _ := reader.ReadByte()
+		buf[c] = input
+		c++
+		buf[c] = 0
+
+		if buf[c-1] == '\n' {
+			break
 		}
+	}
+
+	if c == len(buf) {
+		_, err = reader.ReadBytes('\n')
+	}
+
+	mem[memPos], err = strconv.Atoi(string(buf))
+
+	if err != nil {
+		fmt.Printf("Error getting input.\n")
+		os.Exit(1)
+	}
+}
+
+func exec(instruction int) {
+	reader := bufio.NewReader(os.Stdin)
+
+	switch instruction {
+
+	// hmm
+	case 0:
+		cmd0()
+	//hmmm
+	case 1:
+		cmd1()
+
+	//hmmmm
+	case 2:
+		cmd2()
+
+	// hmmmmm
+	case 3:
+		cmd3()
+
+	// hmmmmmm
+	case 4:
+		cmd4(reader)
+
+	// hmmmmmmm
+	case 5:
+		mem[memPos]--
+
+	// hmmmmmmmm
+	case 6:
+		mem[memPos]++
+
+	// hmmmmmmmmm
+	case 7:
+		cmd7()
 
 	// hmmmmmmmmmm
 	case 8:
@@ -136,45 +221,15 @@ func exec(instruction int) {
 
 	// hmmmmmmmmmmm
 	case 9:
-		if isRegsVal {
-			mem[memPos] = regsVal
-		} else {
-			regsVal = mem[memPos]
-		}
-		isRegsVal = !isRegsVal
+		cmd9()
 
 	// hmmmmmmmmmmmm
 	case 10:
 		fmt.Printf("%d\n", mem[memPos])
 
-	// hmmmmmmmmmmmm
+	// hmmmmmmmmmmmmm
 	case 11:
-		buf := make([]byte, 100)
-		c := 0
-
-		var err error
-
-		for c < len(buf)-1 {
-			input, _ := reader.ReadByte()
-			buf[c] = input
-			c++
-			buf[c] = 0
-
-			if buf[c-1] == '\n' {
-				break
-			}
-		}
-
-		if c == len(buf) {
-			_, err = reader.ReadBytes('\n')
-		}
-
-		mem[memPos], err = strconv.Atoi(string(buf))
-
-		if err != nil {
-			fmt.Printf("Error getting input.\n")
-			os.Exit(1)
-		}
+		cmd11(reader)
 
 	default:
 		fmt.Printf("Unrecognized command.\n")
@@ -184,6 +239,38 @@ func exec(instruction int) {
 	pc++
 }
 
+func strToCmd(input string, line int) {
+	mcount := strings.Count(input, "m")
+	switch mcount {
+	case 2:
+		program = append(program, 0)
+	case 3:
+		program = append(program, 1)
+	case 4:
+		program = append(program, 2)
+	case 5:
+		program = append(program, 3)
+	case 6:
+		program = append(program, 4)
+	case 7:
+		program = append(program, 5)
+	case 8:
+		program = append(program, 6)
+	case 9:
+		program = append(program, 7)
+	case 10:
+		program = append(program, 8)
+	case 11:
+		program = append(program, 9)
+	case 12:
+		program = append(program, 10)
+	case 13:
+		program = append(program, 11)
+	default:
+		fmt.Printf("ERROR. Invalid command. At Line %d\n", line)
+		os.Exit(1)
+	}
+}
 
 // Interpret reads from file f and puts the cmds into program[].
 func Interpret(f string) {
@@ -208,6 +295,7 @@ func Interpret(f string) {
 	read := true
 
 	var input string
+	line := 0
 
 	for read {
 		n, readerr := reader.ReadBytes('\n')
@@ -219,50 +307,28 @@ func Interpret(f string) {
 
 		input = string(n)
 
-		mcount := strings.Count(input, "m")
-		switch mcount {
-		case 2:
-			program = append(program, 0)
-		case 3:
-			program = append(program, 1)
-		case 4:
-			program = append(program, 2)
-		case 5:
-			program = append(program, 3)
-		case 6:
-			program = append(program, 4)
-		case 7:
-			program = append(program, 5)
-		case 8:
-			program = append(program, 6)
-		case 9:
-			program = append(program, 7)
-		case 10:
-			program = append(program, 8)
-		case 11:
-			program = append(program, 9)
-		case 12:
-			program = append(program, 10)
-		case 13:
-			program = append(program, 11)
-		default:
-			fmt.Printf("ERROR. Invalid command.\n")
+		hCount := strings.Count(input, "h")
+
+		if hCount < 1 {
+			fmt.Printf("Syntax Error, at line %d. Only use hmm...",
+				line)
 			os.Exit(1)
 		}
-	}
 
-	if input[0] != 'h' {
-		fmt.Printf("Syntax Error. Use h%s, not %s\n", input, input)
+		strToCmd(input, line)
+		line++
 	}
 
 
 	//Init main memory
 	mem = append(mem, 0)
-	memPos = mem[0]
+	memPos = 0
 
-	pc = program[0]
+	pc = 0
 
-	for len(program) > 0 {
-		exec(program[0])
+	for pc != len(program) {
+		exec(program[pc])
+		//fmt.Printf("Currently at program[%d]\n", pc)
 	}
+	os.Exit(0)
 }
